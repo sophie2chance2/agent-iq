0bf945bb65518b4855886ef3b1decf00d8d1c9f7
Sophie Chance <74380004+sophie2chance2@users.noreply.github.com>
2025-11-16 13:55:17 -0800

    remove old

--- ui_old/lib/api-client/apiClient.ts
+++
@@ -1,5 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export { DefaultService as apiClient } from './services/DefaultService';
